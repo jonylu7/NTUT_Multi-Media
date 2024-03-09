22f195846679ad3ac12638b8cbf024471466f0c3
jonylu7 <jonylu7@gmail.com>
2024-03-09 16:12:05 +0800

    basic functions

---
+++ HW1/Environment Print.py
@@ -0,0 +1,8 @@
+import cv2
+import platform
+import numpy as np
+
+print("CV2:", cv2.__version__)
+print("NUMPY:", np.__version__)
+print("PYTHON", platform.python_version())
+print("SYSTEM", platform.platform())
